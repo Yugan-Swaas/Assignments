ab28a3a145965ed0dece0acfd10a87cdbde2767d
Yugan-Swaas <yugan.ganessane@swaas.net>
2024-07-19 11:24:24 +0530

    Add files via upload

---
+++ problem_1 (1).js
@@ -0,0 +1,87 @@
+//Categorizing Data
+
+/* Input Json */
+const employeeDataIn = {
+    employee_1:{
+        empid: "101",
+        empname:"Vikram",
+        empexperience: "2",
+        empsalary: "10000",
+        empdepartment: "IT"
+    },
+    employee_2:{
+        empid: "102",
+        empname:"Surya",
+        empexperience: "3",
+        empsalary: "20000",
+        empdepartment: "IT"
+    },
+    employee_3:{
+        empid: "103",
+        empname:"Kamal",
+        empexperience: "5",
+        empsalary: "30000",
+        empdepartment: "Finance"
+    },
+    employee_4:{
+        empid: "104",
+        empname:"Dhanush",
+        empexperience: "2",
+        empsalary: "15000",
+        empdepartment: "Finance"
+    },
+    employee_5:{
+        empid: "105",
+        empname:"Surya",
+        empexperience: "3",
+        empsalary: "20000",
+        empdepartment: "HR"
+    },
+    employee_6:{
+        empid: "106",
+        empname:"Kamal",
+        empexperience: "5",
+        empsalary: "30000",
+        empdepartment: "HR"
+    },
+}
+/* Input Json */
+
+
+/* Logic Implementation Function */
+let employeeDataOut = {}
+function dataCategorizer(data, key){
+
+    /* Write your logic here --- Start */
+
+    /* Write your logic here --- End */
+    console.log(employeeDataOut);
+}
+/* Logic Implementation Function */
+
+
+/* Function Call */
+const categoryKey = 'empexperience'
+const categoryValue = '5'
+dataCategorizer(employeeDataIn, categoryKey)
+/* Function Call */
+
+/* Expected Output */
+/*
+{
+    employee_3:{
+        empid: "103",
+        empname:"Kamal",
+        empexperience: "5",
+        empsalary: "30000",
+        empdepartment: "Finance"
+    },
+    employee_6:{
+        empid: "106",
+        empname:"Kamal",
+        empexperience: "5",
+        empsalary: "30000",
+        empdepartment: "HR"
+    }
+} */
+/* Expected Output */
---
+++ problem_2.js
@@ -0,0 +1,52 @@
+// Calculate Cutoff for Given Marks
+// Check Whether he/she is eligible to apply the course
+
+/* Input */
+const studentMarks = {
+    "maths": 78,
+    "physics": 85,
+    "chemistry": 62,
+    "biology": 90
+};
+/* Input */
+
+
+/* Logic Implementation Function */
+let cutoff = 0
+function calculateCutoff(marksJson) {
+    
+    /* Write your logic here --- Start */
+   let subject1=marksJson.maths
+    console.log(marksJson)
+    
+
+    /* Write your logic here --- End */
+ 
+    // console.log('The cutoff is:'+ cutoff);
+}
+/* Logic Implementation Function */
+
+
+/* Logic Implementation Function */
+const eligibilityCutOff = 75
+function checkEligibility(mark){
+    /* Write your logic here --- Start */
+
+    // this function should return message like "You are Eligible" or "You are not Eligible"
+
+    /* Write your logic here --- End */
+}
+/* Logic Implementation Function */
+
+
+/* Function Call */
+calculateCutoff(studentMarks);
+// checkEligibility(cutoff);
+/* Function Call */
+
+/* Expected Output */
+
+// The cutoff is: 77.80
+// You are Eligible
+
+/* Expected Output */
